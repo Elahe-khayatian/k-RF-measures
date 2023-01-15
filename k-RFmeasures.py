8d569f1f776fea4441c855ab1e28ad6ae3279b2e
Elahe-khayatian <99793020+Elahe-khayatian@users.noreply.github.com>
2023-01-15 17:55:00 +0800

    Update k-RFmeasures.py

--- k-RFmeasures.py
+++ k-RFmeasures.py
@@ -2,12 +2,12 @@ import functions
 import argparse
 import json
 """
-k-RFmeasures.py computes pairwise k-RF distances of all (rooted) trees in an input file in which each tree is represented by its (directed) edges.
+k-RFmeasures.py computes pairwise k-RF scores of all (rooted) trees in an input file in which each tree is represented by its (directed) edges.
 By default, it considers trees as unrooted trees.
 For unrooted trees, the command line is 'python3 k-RFmeasures.py inputfile k', and for rooted trees, it is 'python3 k-RFmeasures.py -r inputfile k'.
 """
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Find pairwise k-RF distances of a set of trees.')
+    parser = argparse.ArgumentParser(description='Find pairwise k-RF scores of a set of trees.')
     parser.add_argument('inputfile')
     parser.add_argument('k')
     parser.add_argument('-r', '--rooted', action='store_true')
